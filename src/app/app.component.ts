1e4c4c3b1663cffa9827fbd075e8ccde6942c2ce
dymafr <erwan1309@hotmail.com>
2021-01-08 12:51:14 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
+import { state, style, trigger } from "@angular/animations";
 import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+  styleUrls: ["./app.component.scss"],
+  animations: [
+    trigger("square", [state("normal", style({ backgroundColor: "blue" }))])
+  ]
 })
 export class AppComponent implements OnInit {
+  public state = "normal";
+
   ngOnInit() {}
 }
